b4009117b52ebf10949c794c935536c4cae8b798
jasmin-29 <jasmine.sayyad.29@gmail.com>
2024-11-07 15:59:28 +0530

    manage tenders by email

--- frontend/src/services/tenderService.js
+++ frontend/src/services/tenderService.js
@@ -6,7 +6,17 @@ const API_URL = 'http://localhost:5000/api/tenders'; // Update this URL based on
 // Fetch all tenders
 export const fetchTenders = async () => {
   try {
-    const response = await axios.get(API_URL);
+    const response = await axios.get(`API_URL`);
+    return response.data; // Return the list of tenders
+  } catch (error) {
+    console.error('Error fetching tenders:', error);
+    throw error;
+  }
+};
+
+export const fetchTendersbymail = async (email) => {
+  try {
+    const response = await axios.get(`${API_URL}/${email}`); // Corrected API URL
     return response.data; // Return the list of tenders
   } catch (error) {
     console.error('Error fetching tenders:', error);
